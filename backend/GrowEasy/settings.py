24e06c12a1c65ea44984f1d681299bbff545da34
GaganDeshbhandari <deshbhandarigagan07@gmail.com>
2026-03-15 23:37:54 +0530

    Configured Settings for the backend

--- backend/GrowEasy/settings.py
+++ backend/GrowEasy/settings.py
@@ -14,10 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGOKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG','False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
