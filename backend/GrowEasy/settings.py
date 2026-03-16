3a930b3b41ee965d0d534986ea583ac0bd60602c
GaganDeshbhandari <deshbhandarigagan07@gmail.com>
2026-03-16 12:30:35 +0530

    Configured settings for railway deployment

--- backend/GrowEasy/settings.py
+++ backend/GrowEasy/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG','False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
