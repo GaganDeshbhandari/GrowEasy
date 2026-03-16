41efce85fd30fd7549ec5d63dc3cb573064920fc
GaganDeshbhandari <deshbhandarigagan07@gmail.com>
2026-03-16 13:59:12 +0530

    Configured settings

--- backend/GrowEasy/settings.py
+++ backend/GrowEasy/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files configuration
 # MEDIA_URL = '/media/'
