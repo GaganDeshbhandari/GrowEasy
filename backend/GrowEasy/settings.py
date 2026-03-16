b03657dfa65dac21dc8eac90b97ac2bbee329a03
GaganDeshbhandari <deshbhandarigagan07@gmail.com>
2026-03-16 12:35:45 +0530

    Installed whitenoise and configured middlewares

--- backend/GrowEasy/settings.py
+++ backend/GrowEasy/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files configuration
 # MEDIA_URL = '/media/'
