f026f4ee97fa4e9006bc79d80ac20eb3cafa04b8
GaganDeshbhandari <deshbhandarigagan07@gmail.com>
2026-03-16 16:39:04 +0530

    Added Frontend hosted link to the settings.py

--- backend/GrowEasy/settings.py
+++ backend/GrowEasy/settings.py
@@ -176,6 +176,7 @@ else:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://grow-easy-taupe.vercel.app",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
